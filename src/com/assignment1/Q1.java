f058fd8a733cc728fd28bde00a4ea0655ced68aa
srahul22 <rahul20456@gmail.com>
2023-04-11 00:05:39 +0530

    First Question of Assign 1 solved

---
+++ src/com/assignment1/Q1.java
@@ -0,0 +1,16 @@
+package com.assignment1;
+
+public class Q1 {
+    public static boolean checkPrime(int num){
+        for (int i = 2; i < Math.sqrt(num); i++) {
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPrime(18));
+    }
+}
